0948fbb0a84639355247e9b7db9c7f39ae5f6211
Jean-Bombeur Reborn <barackaafrittes@gmail.com>
2019-06-23 06:09:23 +0900

    Player Now Changes Collor to reflect Pose

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -15,7 +15,8 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         canMove = true;
-        
+
+        playerAnim = GetComponent<Animator>();
         //anim.SetBool("monBool", true/false);
     }
 
@@ -26,7 +27,31 @@ public class PlayerController : MonoBehaviour
     {
         timePassed = Time.time;
     }
+    
+    private bool inputReceived = false;
+    IEnumerator QuestionClock()
+    {
+        var timeSinceQuestion = Time.time - timePassed;
+        if (inputReceived)
+        {
+            if (timeSinceQuestion <= 5)
+            {
+                //Small scale reaction
+            }
+            else if (timeSinceQuestion <= 15)
+            {
+                //Medium scale reaction
+            }
+            else
+            {
+                //All Hell is breaking loose, please wait warmly.
+            }
 
+            //Probablement stopper la coroutine en vrai
+        }
+        yield return new WaitForSeconds(1f); //L'appellera 1fois/seconde
+    }
+    
     // Update is called once per frame
     void Update()
     {
@@ -34,15 +59,24 @@ public class PlayerController : MonoBehaviour
         {
             if (Input.GetMouseButtonDown(0)) // left button
             {
-                canMove = false;
+                //canMove = false;
+                playerAnim.SetTrigger("ToAssis");
+                playerAnim.ResetTrigger("ToCouche");
+                playerAnim.ResetTrigger("ToDebout");
             }
             else if (Input.GetMouseButtonDown(1)) // right
             {
-                canMove = false;
+                //canMove = false;
+                playerAnim.SetTrigger("ToCouche");
+                playerAnim.ResetTrigger("ToAssis");
+                playerAnim.ResetTrigger("ToDebout");
             }
             else if (Input.GetMouseButtonDown(2)) // middle btn
             {
-                canMove = false;
+                //canMove = false;
+                playerAnim.SetTrigger("ToDebout");
+                playerAnim.ResetTrigger("ToCouche");
+                playerAnim.ResetTrigger("ToAssis");
             }
         }
     }

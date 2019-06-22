91ce102ef5e6f9620b832134368d6d6ac34f99c0
Santoro Fabrizio <fabrizio1996@live.fr>
2019-06-22 14:52:35 +0200

    Locking mouse and hiding it

--- Assets/Scripts/CameraRotate.cs
+++ Assets/Scripts/CameraRotate.cs
@@ -8,6 +8,9 @@ public class CameraRotate : MonoBehaviour
 
     private void Start()
     {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        
         var position = player.position;
         transform.position = new Vector3(position.x - 20f, y: position.y + pitch, position.z - 20f);
     }
@@ -19,5 +22,11 @@ public class CameraRotate : MonoBehaviour
         transform.LookAt(position);
         
         transform.RotateAround(position, Vector3.up, Input.GetAxisRaw("Mouse X") * turnSpeed);
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
     }
 }
